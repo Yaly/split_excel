4cbbdddea09cab2f3a87e36b5f429c8f677add42
yaly <yaly.zhao@gmail.com>
2017-02-21 11:40:59 +0800

    You can specify the filename and the number of rows per file from command line.

--- split_excel.py
+++ split_excel.py
@@ -3,7 +3,7 @@
 # @Author: YALY
 # @Date:   2017-01-13 15:11:17
 # @Last Modified by:   yaly
-# @Last Modified time: 2017-02-09 14:58:35
+# @Last Modified time: 2017-02-21 11:39:14
 # Version 0.1
 
 '''
@@ -13,9 +13,19 @@ The number of rows per file is 100 by default, you can custom this too.
 '''
 
 from openpyxl import Workbook, load_workbook
+import argparse
+
+
+# define argumnts
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', dest='file_num', default=100, help='specifiy the number of rows per file')
+parser.add_argument('-f', dest='file_name', required = True, help='specify the Excel file')
+
+args = parser.parse_args()
+
 
 # Load xlsx file
-wb1 = load_workbook('wb.xlsx')
+wb1 = load_workbook(args.file_name)
 
 # read worksheet
 ws1 = wb1.active
@@ -26,7 +36,7 @@ ws1_columns = len(tuple(ws1.columns))
 
 
 # set the number of rows per file
-sep_rows = 1000
+sep_rows = args.file_num
 
 
 def insert_data(file_num, columns, rows=100):
